d617f63995a7fe5e5d81923eb176e2c9f3a16fca
Valravn87 <ca11887@hotmail.com>
2019-02-18 03:06:10 -0500

    Add files via upload

---
+++ https:/github.com/Valravn87/CapstoneProject/Scripts/accordion.js
@@ -0,0 +1,16 @@
+﻿var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+			panel.style.padding = "0px";
+		} else {
+			panel.style.maxHeight = (panel.scrollHeight + 80) + "px";
+			panel.style.padding = "10px 18px 18px 10px";
+		}
+	});
+}
---
+++ https:/github.com/Valravn87/CapstoneProject/Scripts/stockchange.js
@@ -0,0 +1,7 @@
+﻿function StockLevelChanged(id) {
+	var select = document.getElementById("new-stock" + id);
+	var url = "/RestaurantItems/ChangeStock/" + id + "?code=" + select.value;
+	$.ajax({ method: "POST", url: url }).done(function (data) {
+		$("#item-id-row" + id).attr("class", "item-row" + select.value);
+	});
+}
